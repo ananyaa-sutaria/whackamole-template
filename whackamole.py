d7a0c3b5e3cd46512ad437716521def86faf1d32
anyaasutaria <ananyaa.sutaria@gmail.com>
2024-11-12 16:31:06 -0500

    checking if sync is working

--- whackamole.py
+++ whackamole.py
@@ -1,14 +1,23 @@
 import pygame
 
 
+
 def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+
+        #pygame.draw.line(screen, (0,0,0), (x1, y1), (x2, y2))
+
+        width = 20
+        height = 16
+        square_size = 32
+
+        screen.blit(mole_image, mole_image.get_rect(topleft=(1, 2)))
         running = True
         while running:
             for event in pygame.event.get():
